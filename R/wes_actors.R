4952d82c472138f0e4438859101da3b8234d1fe4
KellyTall <42193386+KellyTall@users.noreply.github.com>
2025-05-29 15:40:35 +1000

    updated files
    for CRAN submission

--- R/wes_actors.R
+++ R/wes_actors.R
@@ -1,16 +1,17 @@
 #' Cast List from Wes Anderson Films
 #'
-#' A dataset of actors appearing in films directed by Wes Anderson. Includes film title, actor name, release year, runtime in minutes, and worldwide box office takings.
+#' A dataset of actors appearing in films directed by Wes Anderson.
+#' Includes film title, actor name, release date, runtime, and worldwide box office takings.
 #'
-#' @format A tibble with 5 variables:
+#' @format A tibble with 353 rows and 5 variables:
 #' \describe{
 #'   \item{film}{Film title}
 #'   \item{actor}{Actor name}
 #'   \item{release_date}{Date of first screening}
 #'   \item{box_office_usd}{Worldwide box office takings in USD}
-#'   \item{duration_period}{Duration of the film in hh:mm:ss}
+#'   \item{duration_period}{Duration of the film in hours and minutes}
 #' }
-#' @source Wikidata. Query via \url{https://www.wikidata.org}
+#' @source \href{https://www.wikidata.org/wiki/Wikidata:Main_Page}{Wikidata}
 #' @examples
 #' dplyr::count(wes_actors, actor, sort = TRUE)
 "wes_actors"
--- data-raw/wes_actors.R
+++ data-raw/wes_actors.R
@@ -32,7 +32,7 @@ wes_actors <- results |>
   clean_names() |>
   filter(!film_label %in% c("Castello Cavalcanti", "Cousin Ben Troop Screening with Jason Schwartzman", "Hotel Chevalier")) |> ##removing shorts / promos
   filter(duration != "13") |> # filters out short film versions of bottle rocket
-  mutate(release_date = ymd(release_date)) |>
+  mutate(release_date = lubridate::ymd(release_date)) |>
   mutate(actor_label = case_when(actor_label == "Q1333118" ~ "Richard Ayoade",
                                  TRUE ~ actor_label)) |>
   group_by(film_label, actor_label) |>
@@ -41,7 +41,7 @@ wes_actors <- results |>
   mutate(duration_seconds = as.numeric(duration) * 60,
          duration_period = seconds_to_period(duration_seconds)
   ) |>
-  select(-duration, -duration_seconds) |> 
+  select(-duration, -duration_seconds) |>
   rename(film = film_label,
          actor=actor_label,
          box_office_usd = box_office)
